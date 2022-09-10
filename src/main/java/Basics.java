d97aadc046e5d505e1064edba72525e91c8ccb0b
Hykilpikonna <me@hydev.org>
2022-09-09 23:11:35 -0400

    [U] Done

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -7,6 +7,10 @@
  * relevant readings for each task.
  */
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  *  1. In Java, no code can exist outside of a class. Unlike Python which uses
  *    functions, all code in Java uses methods.
@@ -40,10 +44,10 @@ public class Basics {
          */
         System.out.println(7 + 5);
 
-        /* TODO (Task 1): Write a line of code below that prints the string
+        /* done (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
-
+        System.out.println("Hello World!");
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -59,10 +63,11 @@ public class Basics {
          */
 
         /*
-         * TODO (Task 2): Create a variable named my_variable and assign it the
+         * done (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        var my_variable = 100;
+        // but just why would you name something in snake_case when everything else is camelCase in Java?
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -88,7 +93,7 @@ public class Basics {
          */
 
         /*
-         * TODO (Task 3): Create a for-loop that loops from 10 to 0, printing
+         * done (Task 3): Create a for-loop that loops from 10 to 0, printing
          *                the following at each iteration:
          *                Current count: #
          *
@@ -99,8 +104,7 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
-
+        IntStream.range(0, 11).forEach(i -> System.out.printf("Current count: %d\n", 10 - i));
     }
 
     /**
@@ -133,18 +137,21 @@ public class Basics {
      * @return           The first letter of every word in to_split
      */
     public static String split(String to_split) {
-        /* TODO (Task 4): Complete this method body.
+        /* done (Task 4): Complete this method body.
          *                The String methods .split and .charAt may be helpful,
          *                along with the StringBuilder.append
          *                You may also assume that to_split always has 7 words
          *                separated by spaces, so no loops are required, though
          *                you may use them if you wish.
          */
-        StringBuilder ret = new StringBuilder();
+//        StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        return Arrays.stream(to_split.split(" "))
+            .map(word -> "" + word.charAt(0))
+            .collect(Collectors.joining());
 
-        return ret.toString();
+//        return ret.toString();
     }
 
     /**
@@ -164,19 +171,21 @@ public class Basics {
     public static int oddSum(int[] arr) {
         int current_sum = 0;
 
-        /* TODO (Task 5): Complete this method body using a for-loop.
+        /* done (Task 5): Complete this method body using a for-loop.
          *                You can find the length of an array by using the
          *                .length attribute (e.g. arr.length)
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        for (int i = 1; i < arr.length; i += 2)
+            current_sum += arr[i];
 
         return current_sum;
     }
 
 
     /*
-     * TODO (Task 6): Submit the changes you made on GitHub!
+     * done (Task 6): Submit the changes you made on GitHub!
      *                When you submit it, go to the 'Actions' tab. You should
      *                be able to see this exercise being autograded!
      *
